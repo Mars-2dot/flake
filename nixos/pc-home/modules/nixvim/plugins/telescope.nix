de54e873de0be40ef08effd05d71ed31e1e29313
Ilya Boldyrev <>
2024-07-11 23:28:48 +0300

    update

--- nixos/pc-home/modules/nixvim/plugins/telescope.nix
+++ nixos/pc-home/modules/nixvim/plugins/telescope.nix
@@ -2,28 +2,28 @@
   programs.nixvim.plugins.telescope = {
     enable = true;
 
-    keymaps = {
-      "<leader>ff" = "find_files";
-      "<leader>b" = "buffers";
-      "<leader>fh" = "help_tags";
-      "<leader>gf" = "git_files";
-      "<leader>of" = "oldfiles";
+#    keymaps = {
+#      "<leader>ff" = "find_files";
+#      "<leader>b" = "buffers";
+#      "<leader>fh" = "help_tags";
+#      "<leader>gf" = "git_files";
+#      "<leader>of" = "oldfiles";
       # "<leader>fg" = "live_grep";
       # "<leader>fd" = "diagnostics";
-    };
+#    };
 
-    keymapsSilent = true;
+#    keymapsSilent = true;
 
-    settings.defaults = {
-      file_ignore_patterns = [
-        "^.git/"
-        "^.mypy_cache/"
-        "^__pycache__/"
-        "^output/"
-        "^data/"
-        "%.ipynb"
-      ];
-      set_env.COLORTERM = "truecolor";
-    };
+#    settings.defaults = {
+#      file_ignore_patterns = [
+#        "^.git/"
+#        "^.mypy_cache/"
+#        "^__pycache__/"
+#        "^output/"
+#        "^data/"
+#        "%.ipynb"
+#      ];
+#      set_env.COLORTERM = "truecolor";
+#    };
   };
 }
