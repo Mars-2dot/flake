a1bedd9c6627f21b414f3f7e642281d932e27396
Ilya Boldyrev <>
2024-07-12 00:41:16 +0300

    update

--- nixos/pc-home/configuration.nix
+++ nixos/pc-home/configuration.nix
@@ -10,9 +10,9 @@
   ];
 
   nixpkgs.overlays = [
-      (_: prev: {
-          python312 = prev.python312.override { packageOverrides = _: pysuper: { nose = pysuper.pynose; }; };
-      })
+#      (_: prev: {
+#          python312 = prev.python312.override { packageOverrides = _: pysuper: { nose = pysuper.pynose; }; };
+#      })
       inputs.polymc.overlay
   ];
 
